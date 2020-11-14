23c643df07a17eded2d0d8854d5bf50cedc083d9
Song-i <elephant890@naver.com>
2020-11-15 02:39:59 +0900

    song-i maked mainActivity and get permission of camera.

--- app/src/main/java/com/example/balance/MainActivity.java
+++ app/src/main/java/com/example/balance/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.balance;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton CameraButton, GraphButton;
+    TextView TutorialButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Action Bar(Title bar) 받아와서 없애기*/
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+
+        /*button 할당*/
+        CameraButton = (ImageButton) findViewById(R.id.CameraButton);
+        GraphButton = (ImageButton) findViewById(R.id.GraphButton);
+        TutorialButton = (TextView) findViewById(R.id.TutorialButton);
+
+        CameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
